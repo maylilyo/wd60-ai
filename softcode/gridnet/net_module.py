9117ad31e3b1107d54b754616d18c9cacafa7837
maylilyo <maylily_o@naver.com>
2021-08-16 17:03:34 +0900

    add new comment
    Former-commit-id: 0333419606f7e9386431332b53ce092e5451759a [formerly 0333419606f7e9386431332b53ce092e5451759a [formerly ae6ed09b5ab3cfc779eb3f5a2008d1a0c941b1d3]]
    Former-commit-id: 38e85c2a191c570c79b85eceef8d479d08afd019
    Former-commit-id: 2df69f035b88adc44ff35f14ad799177a269d0cc

--- softcode/gridnet/net_module.py
+++ softcode/gridnet/net_module.py
@@ -47,24 +47,23 @@ class GridNet(nn.Module):
         # x_l3: [num_batches, 192, 64, 112]
 
         state_00 = self.lateral_init(x_l1)
-        # state_00: [num_batches, 32, 256, 448]
-
         state_10 = self.down_0_0(x_l2)
-        # state_10: [num_batches, 64, 128, 224]
-
         state_20 = self.down_1_0(x_l3)
-        # state_20: [num_batches, 96, 64, 112]
 
         # 01의 입력을 warp로 바꾼 결과
+        # Down Block Stage
         state_01 = self.lateral_0_0(state_00)
-        #[num_batches, 32, 256, 448]
         state_11 = self.down_0_1(state_01) + self.lateral_1_0(state_10)
         state_21 = self.down_1_1(state_11) + self.lateral_2_0(state_20)
 
         state_02 = self.lateral_0_1(state_01)
         state_12 = self.down_0_2(state_02) + self.lateral_1_1(state_11)
         state_22 = self.down_1_2(state_12) + self.lateral_2_1(state_21)
+        # state_00, 01, 02 : [num_batches, 32, 256, 448]
+        # state_10, 11, 12 : [num_batches, 64, 128, 224]
+        # state_20, 21, 22 : [num_batches, 96, 64, 112]
 
+        # Up Block Stage
         state_23 = self.lateral_2_2(state_22)
         state_13 = self.up_1_0(state_23) + self.lateral_1_2(state_12)
         state_03 = self.up_0_0(state_13) + self.lateral_0_2(state_02)
@@ -76,6 +75,11 @@ class GridNet(nn.Module):
         state_25 = self.lateral_2_4(state_24)
         state_15 = self.up_1_2(state_25) + self.lateral_1_4(state_14)
         state_05 = self.up_0_2(state_15) + self.lateral_0_4(state_04)
+        # state_23, 24, 25 : [num_batches, 96, 64, 112]
+        # state_13, 14, 15 : [num_batches, 64, 128, 224]
+        # state_03, 04, 05 : [num_batches, 32, 256, 448]
+
+        print(self.lateral_final(state_05).shape)
         return self.lateral_final(state_05)
 
 
