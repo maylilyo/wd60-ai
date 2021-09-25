37ad4ed24cf5466e2a0193f72d10ca456b8c8cee
sehandev <sehandev@gmail.com>
2021-09-25 18:27:25 +0900

    REFACTOR: RIFE laplacian

--- RIFE/model/laplacian.py
+++ RIFE/model/laplacian.py
@@ -1,11 +1,9 @@
 import torch
-import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
+import nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-import torch
 
 def gauss_kernel(size=5, channels=3):
     kernel = torch.tensor([[1., 4., 6., 4., 1],
@@ -18,22 +16,30 @@ def gauss_kernel(size=5, channels=3):
     kernel = kernel.to(device)
     return kernel
 
+
 def downsample(x):
     return x[:, :, ::2, ::2]
 
+
 def upsample(x):
-    cc = torch.cat([x, torch.zeros(x.shape[0], x.shape[1], x.shape[2], x.shape[3]).to(device)], dim=3)
+    tmp = torch.zeros(x.shape[0], x.shape[1], x.shape[2], x.shape[3]).to(device)
+    cc = torch.cat([x, tmp], dim=3)
     cc = cc.view(x.shape[0], x.shape[1], x.shape[2]*2, x.shape[3])
-    cc = cc.permute(0,1,3,2)
-    cc = torch.cat([cc, torch.zeros(x.shape[0], x.shape[1], x.shape[3], x.shape[2]*2).to(device)], dim=3)
+    cc = cc.permute(0, 1, 3, 2)
+
+    tmp = torch.zeros(x.shape[0], x.shape[1], x.shape[3], x.shape[2] * 2).to(device)
+    cc = torch.cat([cc, tmp], dim=3)
     cc = cc.view(x.shape[0], x.shape[1], x.shape[3]*2, x.shape[2]*2)
-    x_up = cc.permute(0,1,3,2)
-    return conv_gauss(x_up, 4*gauss_kernel(channels=x.shape[1]))
+    x_up = cc.permute(0, 1, 3, 2)
+
+    return conv_gauss(x_up, 4 * gauss_kernel(channels=x.shape[1]))
+
 
 def conv_gauss(img, kernel):
-    img = torch.nn.functional.pad(img, (2, 2, 2, 2), mode='reflect')
-    out = torch.nn.functional.conv2d(img, kernel, groups=img.shape[1])
+    img = F.pad(img, (2, 2, 2, 2), mode='reflect')
+    out = F.conv2d(img, kernel, groups=img.shape[1])
     return out
+
 
 def laplacian_pyramid(img, kernel, max_levels=3):
     current = img
@@ -47,13 +53,14 @@ def laplacian_pyramid(img, kernel, max_levels=3):
         current = down
     return pyr
 
-class LapLoss(torch.nn.Module):
+
+class LapLoss(nn.Module):
     def __init__(self, max_levels=5, channels=3):
-        super(LapLoss, self).__init__()
+        super().__init__()
         self.max_levels = max_levels
         self.gauss_kernel = gauss_kernel(channels=channels)
-        
+
     def forward(self, input, target):
-        pyr_input  = laplacian_pyramid(img=input, kernel=self.gauss_kernel, max_levels=self.max_levels)
+        pyr_input = laplacian_pyramid(img=input, kernel=self.gauss_kernel, max_levels=self.max_levels)
         pyr_target = laplacian_pyramid(img=target, kernel=self.gauss_kernel, max_levels=self.max_levels)
-        return sum(torch.nn.functional.l1_loss(a, b) for a, b in zip(pyr_input, pyr_target))
+        return sum(F.l1_loss(a, b) for a, b in zip(pyr_input, pyr_target))
